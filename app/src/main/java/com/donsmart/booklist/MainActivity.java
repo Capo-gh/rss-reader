7355cf0b1f36bba3d1ce43e8d484a06d860aba06
Capo-gh <sabikuibrahim@gmail.com>
2020-08-11 19:45:39 +0800

    First commit

---
+++ app/src/main/java/com/donsmart/booklist/MainActivity.java
@@ -0,0 +1,165 @@
+package com.donsmart.booklist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.IntentService;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvRss;
+    ArrayList<String> titles;
+    ArrayList<String> links;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvRss = (ListView) findViewById(R.id.lvRss);
+
+        titles = new ArrayList<String>();
+        links = new ArrayList<String>();
+
+        lvRss.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Uri uri = Uri.parse(links.get(position));
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+
+            }
+        });
+
+        new ProcessInBackground().execute();
+    }
+
+
+
+    public InputStream getInputStream(URL url)
+    {
+        try
+        {
+            return url.openConnection().getInputStream();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+    public class ProcessInBackground extends AsyncTask<Integer,Void,Exception>
+    {
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog.setMessage("Busy loading rss feed...please wait...");
+        }
+
+        @Override
+        protected Exception doInBackground(Integer... integers) {
+
+           try
+           {
+               URL url = new URL("http://feeds.news24.com/articles/fin24/tech/rss");
+
+               XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+               factory.setNamespaceAware(false);
+
+               XmlPullParser xpp = factory.newPullParser();
+
+               xpp.setInput(getInputStream(url), "UTF_8");
+
+               boolean insideItem = false;
+
+               int evenType = xpp.getEventType();
+
+               while (evenType != XmlPullParser.END_DOCUMENT)
+               {
+                   if (evenType == XmlPullParser.START_TAG)
+                   {
+                       if (xpp.getName().equalsIgnoreCase("item"))
+                       {
+                           insideItem = true;
+                       }
+                       else if (xpp.getName().equalsIgnoreCase("title"))
+                       {
+                           if (insideItem)
+                           {
+                               titles.add(xpp.nextText());
+
+                           }
+                       }
+                       else if (xpp.getName().equalsIgnoreCase("link"))
+                       {
+                           if (insideItem)
+                           {
+                               links.add(xpp.nextText());
+                           }
+                       }
+                   }
+                   else  if (evenType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item"))
+                   {
+                       insideItem = false;
+                   }
+
+                   evenType = xpp.next();
+               }
+
+           }
+           catch (MalformedURLException e)
+           {
+               exception = e;
+           }
+           catch (XmlPullParserException e)
+           {
+               exception = e;
+           }
+           catch (IOException e)
+           {
+               exception = e;
+           }
+
+            return exception;
+        }
+
+
+        @Override
+        protected void onPostExecute(Exception s) {
+            super.onPostExecute(s);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                    android.R.layout.simple_list_item_1,titles);
+
+            lvRss.setAdapter(adapter);
+
+
+
+            progressDialog.dismiss();
+        }
+    }
+}
